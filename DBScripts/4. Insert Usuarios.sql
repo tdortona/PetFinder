9bc570b940b479b1322d40233ebb24dd0b2bd6e6
Matias Martinez <martinezmsms@gmail.com>
2018-11-17 16:10:11 -0300

    modificaciones en BDD

--- DBScripts/4. Insert Usuarios.sql
+++ DBScripts/4. Insert Usuarios.sql
@@ -1,11 +1,11 @@
-INSERT INTO Usuario
-VALUES ('', 'Tomas Dortona', 'tdortona@gmail.com', 'Palacios 374', '1137809082')
+INSERT INTO Usuario (Nombre, Email, Direccion, TelefonoContacto, IdRedSocial, IdUsuarioRedSocial)
+VALUES ('Tomas Dortona', 'tdortona@gmail.com', 'Palacios 374', '1137809082', '1', 'id1')
 
-INSERT INTO Usuario
-VALUES ('', 'Rocio Bareiro', 'bareiro.rsb@gmail.com', '', '1155611126')
+INSERT INTO Usuario (Nombre, Email, Direccion, TelefonoContacto, IdRedSocial, IdUsuarioRedSocial)
+VALUES ('Rocio Bareiro', 'bareiro.rsb@gmail.com', '', '1155611126', '1', 'id2')
 
-INSERT INTO Usuario
-VALUES ('', 'Matias Martinez', 'martinezmsms@gmail.com', '', '1150560969')
+INSERT INTO Usuario (Nombre, Email, Direccion, TelefonoContacto, IdRedSocial, IdUsuarioRedSocial)
+VALUES ('Matias Martinez', 'martinezmsms@gmail.com', 'Maipu 3835', '1150560969', '2', 'id3')
 
-INSERT INTO Usuario
-VALUES ('', 'Jorge Velazquez', 'j.velazquez1985@gmail.com', '', '1168744236')
+INSERT INTO Usuario (Nombre, Email, Direccion, TelefonoContacto, IdRedSocial, IdUsuarioRedSocial)
+VALUES ('Jorge Velazquez', 'j.velazquez1985@gmail.com', '', '1168744236', '2', 'id4')
