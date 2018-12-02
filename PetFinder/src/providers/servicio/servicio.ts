a99425cd9f3dcc77c98df9d67dd2d37aa1cd8f25
Tomas <tomasdo@lagash.com>
2018-12-02 12:22:41 -0300

    Subo script de insert

--- PetFinder/src/providers/servicio/servicio.ts
+++ PetFinder/src/providers/servicio/servicio.ts
@@ -127,8 +127,8 @@ export class ServicioProvider {
 
   showAlertExito(clase: string, score: number) {
     const alert = this.alertCtrl.create({
-      title: 'Gracias por colaborar!',
-      subTitle: 'Resultado:<ul><li>Clase: ' + clase + '</li><li>Probabilidad: ' + score * 100 + '%</li></ul>',
+      title: '¡Ya recibimos tu foto!',
+      subTitle: 'Gracias por colaborar con CanFind.',
       buttons: [{
         text: 'OK',
         handler: () => {
@@ -142,8 +142,8 @@ export class ServicioProvider {
 
   showAlertError() {
     const alert = this.alertCtrl.create({
-      title: 'Gracias por colaborar!',
-      subTitle: 'Por el momento no encontramos un resultado aproximado. Podes intentar con otra foto.',
+      title: '¡Ya recibimos tu foto!',
+      subTitle: 'Gracias por colaborar con CanFind.',
       buttons: [{
         text: 'OK',
         handler: () => {
